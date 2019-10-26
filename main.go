7247531f9c62de34b13e50aa30f9925e6fccdcb8
金飞 <28206254@qq.com>
2019-10-26 14:16:11 +0800

    s

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package rset
 
 import (
-	"github.com/ascode/rset/collection"
-	"github.com/ascode/rset/test_data"
+	//"github.com/ascode/rset/collection"
+	//"github.com/ascode/rset/test_data"
 	_ "github.com/ascode/rset/test_data"
 )
 
 func main() {
-	collection.NewSet(test_data.Students)
+	//collection.NewSet(test_data.Students)
 }
